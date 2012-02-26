31a1cdcc0f8720568d280c393db77c0e0f0d10d5
CAMongrel <camongrel@yahoo.com>
2012-02-26 17:07:43 +0100

    Initial commit

---
+++ MongrelWebServer/SQLWebProxy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+using System.Web;
+
+namespace MongrelWebServer
+{
+	class SQLWebProxy
+	{
+		private Webserver webserver;
+
+		public event LogOutputDelegate OnLogOutput;
+
+		public SQLWebProxy()
+		{
+			webserver = new Webserver();
+			webserver.OnLogOutput += (line) =>
+				{
+					if (OnLogOutput != null)
+						OnLogOutput(line);
+				};
+			webserver.OnReceiveRequest += webserver_OnReceiveRequest;
+			webserver.AddPrefix("http://localhost:8080/");
+		}
+
+		string webserver_OnReceiveRequest(System.Net.HttpListenerRequest request)
+		{
+			Uri uri = request.Url;
+			NameValueCollection parameters = HttpUtility.ParseQueryString(uri.Query);
+			try
+			{
+				string query = parameters["query"];
+			}
+			catch (Exception ex)
+			{
+
+			}
+
+			string responseString = "<xml></xml>";
+
+			return responseString;
+		}
+
+		internal void Start()
+		{
+			webserver.Start();
+		}
+
+		internal void Stop()
+		{
+			webserver.Stop();
+		}
+	}
+}
---
+++ MongrelWebServer/Webserver.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading;
+using System.Web;
+using System.Collections.Specialized;
+
+namespace MongrelWebServer
+{
+	public delegate void LogOutputDelegate(string logLine);
+	public delegate string ReceiveRequestDelegate(HttpListenerRequest request);
+
+	class Webserver
+	{
+		private HttpListener listener;
+		private Thread listenThread;
+
+		public event LogOutputDelegate OnLogOutput;
+		public event ReceiveRequestDelegate OnReceiveRequest;
+
+		public Webserver()
+		{
+			listener = new HttpListener();
+		}
+
+		public void AddPrefix(string prefix)
+		{
+			listener.Prefixes.Add(prefix);
+		}
+
+		public void Start()
+		{
+			if (listener.IsListening)
+				listener.Stop();
+
+			listener.Start();
+
+			listenThread = new Thread(new ThreadStart(ListenThreadFunc));
+			listenThread.Start();
+		}
+
+		public void Stop()
+		{
+			if (listener.IsListening)
+			{
+				listenThread.Abort();
+				listenThread = null;
+
+				listener.Stop();
+			}
+		}
+
+		private void ListenThreadFunc()
+		{
+			try
+			{
+				AddLog("Started listening ...");
+
+				while (true)
+				{
+					HttpListenerContext context = listener.GetContext();
+
+					HttpListenerRequest request = context.Request;
+
+					// Construct a response.
+					string responseString = "<HTML><BODY>";
+					responseString += request.RawUrl;
+					responseString += "</BODY></HTML>";
+
+					if (OnReceiveRequest != null)
+						responseString = OnReceiveRequest(request);
+
+					// Obtain a response object.
+					HttpListenerResponse response = context.Response;
+					byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+					// Get a response stream and write the response to it.
+					response.ContentLength64 = buffer.Length;
+					System.IO.Stream output = response.OutputStream;
+					output.Write(buffer, 0, buffer.Length);
+					// You must close the output stream.
+					output.Close();
+				}
+			}
+			catch (ThreadAbortException)
+			{
+				// 'Tis ok
+				AddLog("Aborted listening ...");
+			}
+
+			AddLog("Finished listening ...");
+		}
+
+		private void AddLog(string line)
+		{
+			if (OnLogOutput != null)
+				OnLogOutput(line);
+		}
+	}
+}
