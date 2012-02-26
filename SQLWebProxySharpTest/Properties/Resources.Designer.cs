c20df3ab882d863b168b789ebdb12fef6f4cd2fe
CAMongrel <camongrel@yahoo.com>
2012-02-26 17:39:34 +0100

    Changed default namespace and assembly name of test project

--- SQLWebProxySharpTest/Properties/Resources.Designer.cs
+++ SQLWebProxySharpTest/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MongrelWebServer.Properties {
+namespace SQLWebProxySharpTest.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MongrelWebServer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MongrelWebServer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SQLWebProxySharpTest.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- SQLWebProxySharpTest/Properties/Settings.Designer.cs
+++ SQLWebProxySharpTest/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MongrelWebServer.Properties {
+namespace SQLWebProxySharpTest.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
